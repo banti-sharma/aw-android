bdf31011a0926669dc4213d5ee5f5ee212ec1b9f
Banti Sharma <banti.sharma@gmail.com>
2022-02-19 18:26:27 +0530

    Added key

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -9,14 +9,10 @@ if [ -z $ANDROID_HOME ]; then
     exit 1
 fi
 
-if [ -z $storepass ]; then
-    echo "\$storepass and \$keypass need to be set"
-    exit 1
-fi
 
 ./gradlew assembleRelease
 mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
-jarsigner -verbose -sigalg SHA1withRSA -storepass $storepass -keypass $keypass -digestalg SHA1 -keystore android.jks $OUTPUT_NAME activitywatch
+jarsigner -verbose -sigalg SHA1withRSA -storepass graphoun1 -keypass graphoun1 -digestalg RSA -keystore graphoun.jks $OUTPUT_NAME activitywatch
 jarsigner -verify $OUTPUT_NAME
 
 zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
