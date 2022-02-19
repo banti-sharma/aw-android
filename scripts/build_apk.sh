f8bc38f3eea1cda5687b0336601a2f512aebe74c
Banti Sharma <banti.sharma@gmail.com>
2022-02-19 21:29:49 +0530

    Uploading Artifact

--- scripts/build_apk.sh
+++ scripts/build_apk.sh
@@ -13,6 +13,9 @@ fi
 ./gradlew assembleRelease
 mv mobile/build/outputs/apk/release/mobile-release-unsigned.apk $OUTPUT_NAME
 
+jarsigner -verbose -sigalg SHA1withRSA -storepass graphoun1 -keypass graphoun1 -digestalg SHA1 -keystore watcher.jks $OUTPUT_NAME activitywatch
+jarsigner -verify $OUTPUT_NAME
+
 zipalign=$(find $ANDROID_HOME/build-tools -name "zipalign" -print | head -n 1)
 $zipalign -v 4 $OUTPUT_NAME $OUTPUT_NAME.new
 mv $OUTPUT_NAME.new $OUTPUT_NAME
